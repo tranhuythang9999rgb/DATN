5564e7468f46c7cbbe8236e9e765295f5c2e077b
tranhuythang9999rgb <thang.tranhuy@ncc.asia>
2024-08-30 00:08:32 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -22,11 +22,11 @@ function App() {
       {/* <HomePage/> */}
       {/* <CheckQrCode/> */}
       {/* <Login/> */}
-      {/* <CheckLogin/> */}
+      <CheckLogin/>
       {/* <TypeBook/> */}
       {/* <Publishers/> */}
       {/* <AuthorBook/> */}
-      <DetailBuy book_id={7861741} />
+      {/* <DetailBuy book_id={7861741} /> */}
       {/* <AppRouter/> */}
     </div>
   );
