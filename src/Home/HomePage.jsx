521483456ea3803000e4d0cec4ba50ad94f53e33
tranhuythang9999rgb <thang.tranhuy@ncc.asia>
2024-09-07 22:47:41 +0700

    ok add

--- src/Home/HomePage.jsx
+++ src/Home/HomePage.jsx
@@ -425,45 +425,49 @@ function HomePage() {
                     </ul>
                 </div>
 
+                <span>
 
-                <div className='footer-home-page'>
-                    <Row justify="space-evenly" align="middle">
-                        <Col span={4} className='footer-col-home-page'>
-                            <Title level={4}>Thông tin</Title>
-                            <ul>
-                                <li><Link href="/about" target="_blank">Giới thiệu</Link></li>
-                                <li><Link href="/contact" target="_blank">Liên hệ</Link></li>
-                                <li><Link href="/faq" target="_blank">Câu hỏi thường gặp</Link></li>
-                                <li><Link href="/returns" target="_blank">Chính sách đổi trả</Link></li>
-                            </ul>
-                        </Col>
-                        <Col span={4} className='footer-col-home-page'>
-                            <Title level={4}>Dịch vụ khách hàng</Title>
-                            <ul>
-                                <li><Link href="/support" target="_blank">Hỗ trợ khách hàng</Link></li>
-                                <li><Link href="/shipping" target="_blank">Giao hàng</Link></li>
-                                <li><Link href="/payment" target="_blank">Phương thức thanh toán</Link></li>
-                            </ul>
-                        </Col>
-                        <Col span={4} className='footer-col-home-page'>
-                            <Title level={4}>Kết nối với chúng tôi</Title>
-                            <ul>
-                                <li><Link href="https://facebook.com" target="_blank">Facebook</Link></li>
-                                <li><Link href="https://twitter.com" target="_blank">Twitter</Link></li>
-                                <li><Link href="https://instagram.com" target="_blank">Instagram</Link></li>
-                            </ul>
-                        </Col>
-                        <Col span={4} className='footer-col-home-page'>
-                            <Title level={4}>Thông tin liên hệ</Title>
-                            <Text>Hưng Hà Thái Bình, Việt Nam</Text><br />
-                            <Text>Email: contact@bookstore.vn</Text><br />
-                            <Text>Điện thoại: +84 123 456 789</Text>
-                        </Col>
-                    </Row>
-                    <div className='footer-bottom-home-page'>
-                        <Text>© 2024 Sách Việt Nam. Bảo lưu mọi quyền.</Text>
+                    <div className='footer-home-page'>
+                        <Row justify="space-evenly" align="middle">
+                            <Col span={4} className='footer-col-home-page'>
+                                <Title style={{display:'flex' ,justifyContent:'center'}} level={4}>Thông tin</Title>
+                                <ul>
+                                    <li><Link href="/about" target="_blank">Giới thiệu</Link></li>
+                                    <li><Link href="/contact" target="_blank">Liên hệ</Link></li>
+                                    <li><Link href="/faq" target="_blank">Câu hỏi thường gặp</Link></li>
+                                    <li><Link href="/returns" target="_blank">Chính sách đổi trả</Link></li>
+                                </ul>
+                            </Col>
+                            <Col span={4} className='footer-col-home-page'>
+                                <Title style={{display:'flex' ,justifyContent:'center'}} level={4}>Dịch vụ khách hàng</Title>
+                                <ul>
+                                    <li><Link href="/support" target="_blank">Hỗ trợ khách hàng</Link></li>
+                                    <li><Link href="/shipping" target="_blank">Giao hàng</Link></li>
+                                    <li><Link href="/payment" target="_blank">Phương thức thanh toán</Link></li>
+                                </ul>
+                            </Col>
+                            <Col span={4} className='footer-col-home-page'>
+                                <Title style={{display:'flex' ,justifyContent:'center'}} level={4}>Kết nối với chúng tôi</Title>
+                                <ul>
+                                    <li><Link href="https://facebook.com" target="_blank">Facebook</Link></li>
+                                    <li><Link href="https://twitter.com" target="_blank">Twitter</Link></li>
+                                    <li><Link href="https://instagram.com" target="_blank">Instagram</Link></li>
+                                </ul>
+                            </Col>
+                            <Col span={4} className='footer-col-home-page'>
+                                <Title style={{display:'flex' ,justifyContent:'center',marginTop:'-3px'}} level={4}>Thông tin liên hệ</Title>
+                                <Text>Hưng Hà Thái Bình, Việt Nam</Text><br />
+                                <Text>Email: contact@bookstore.vn</Text><br />
+                                <Text>Điện thoại: +84 123 456 789</Text>
+                            </Col>
+                        </Row>
+                        <div className='footer-bottom-home-page'>
+                            <Text>© 2024 Sách Việt Nam. Bảo lưu mọi quyền.</Text>
+                        </div>
                     </div>
-                </div>
+
+                </span>
+
 
             </div>
 
