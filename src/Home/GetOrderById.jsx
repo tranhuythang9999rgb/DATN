f2a5bf1ef973db6ba38114f258b9762300c71ad0
tranhuythang9999rgb <thang.tranhuy@ncc.asia>
2024-09-17 02:44:55 +0700

    ok

--- src/Home/GetOrderById.jsx
+++ src/Home/GetOrderById.jsx
@@ -5,12 +5,8 @@ import ProductCard from '../user/ProductCard';
 import { RiSecurePaymentLine } from 'react-icons/ri';
 
 const GetOrderById = () => {
-    const [order, setOrder] = useState(null);
-    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [localLoading, setLocalLoading] = useState(true); // State to handle local storage loading
     const [listBookJson, setListBookJson] = useState([]);
-    const orderId = localStorage.getItem('order_id') || 0;
     const shippingFee = 30000; // Default shipping fee
     const [api, contextHolder] = notification.useNotification();
     const [buttonLoading, setButtonLoading] = useState(false); // State for button loading
@@ -24,35 +20,6 @@ const GetOrderById = () => {
         });
     };
 
-    useEffect(() => {
-        const fetchOrder = async () => {
-            if (!orderId || orderId === 0) {
-                setError('Vui lòng kiểm tra kết nối mạng hoặc thiết bị');
-                setLoading(false);
-                return;
-            }
-
-            try {
-                const response = await axios.get(`http://127.0.0.1:8080/manager/order/infor?id=${orderId}`);
-                if (response.data.code === 0) {
-                    setOrder(response.data.body);
-                } else {
-                    setError(response.data.message);
-                }
-            } catch (err) {
-                setError('Lỗi khi lấy dữ liệu đơn hàng');
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        const timer = setTimeout(() => {
-            setLocalLoading(false);
-            fetchOrder();
-        }, 2000); // Delay of 2 seconds
-
-        return () => clearTimeout(timer);
-    }, [orderId]);
 
     useEffect(() => {
         const storedList = localStorage.getItem('listbook');
@@ -73,7 +40,7 @@ const GetOrderById = () => {
             localStorage.setItem('listbook', JSON.stringify(updatedCart));
         } else {
             alert('Không thể xóa mục vì danh sách chỉ còn lại một mục');
-            return;    
+            return;
         }
     };
 
@@ -120,79 +87,114 @@ const GetOrderById = () => {
         }
     };
 
-    if (localLoading) return <Spin size="large" tip="Đang tải dữ liệu từ bộ nhớ..." />;
-    if (loading) return <Spin size="large" tip="Đang tải..." />;
+    const handlePaymentOfline = async () => {
+        setButtonLoading(true); // Set loading state to true
+        try {
+            const items = listBookJson.map(item => ({
+                cart_id: item.book_id,
+                book_id: item.id,
+                book_name: item.title,
+                quantity: item.quantity,
+                price: item.price,
+                total_amount: totalWithShipping,
+                url: item.url
+            }));
+            console.log(items);
+            const paymentData = {
+                customer_name: userName,
+                items: JSON.stringify(items)
+            };
+
+            const response = await axios.post('http://127.0.0.1:8080/manager/order/api/pend/offline', paymentData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Cookie': `order_id=${Math.floor(Math.random() * 10000000)}`
+                }
+            });
+
+            if (response.data.code === 0) {
+                setListBookJson([]); // Clear the list of books
+                localStorage.setItem('listbook', JSON.stringify([])); //
+                openNotification('topRight', 'Chuyển hướng đến trang thanh toán', 'Bạn sẽ được chuyển đến trang thanh toán trong giây lát.');
+               
+            }
+        } catch (error) {
+            console.error('Có lỗi xảy ra khi thanh toán:', error);
+            openNotification('topRight', 'Lỗi', 'Có lỗi xảy ra trong quá trình thanh toán. Vui lòng thử lại.');
+        } finally {
+            setButtonLoading(false); // Set loading state to false after completion
+        }
+    };
+
+
     if (error) return <Alert message="Lỗi" description={error} type="error" />;
 
     return (
         <div>
-            {order ? (
-                <div>
-                    <Form className="form-cart">
-                        <Form.Item>
-                            {listBookJson.map(item => (
-                                <ProductCard
-                                    key={item.id}
-                                    imageUrl={item.files[0]}
-                                    title={item.title}
-                                    price={item.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
-                                    quantity={item.quantity}
-                                    sell="15"
-                                    onDelete={() => handleDeleteItem(item.id)}
-                                />
-                            ))}
-                        </Form.Item>
-                        <Form.Item>
-                            <div className="form-item-total">
-                                <div
-                                    style={{
-                                        backgroundColor: 'white',
-                                        height: '50px',
-                                        fontSize: '20px',
-                                        padding: '10px',
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        borderRadius: '8px',
-                                        marginBottom: '8px',
-                                        boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
-                                    }}
-                                    className="total-product"
-                                >
-                                    Tổng tiền sản phẩm: {totalAmount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
-                                </div>
-                                <div className="total-shipping">
-                                    Phí vận chuyển: {shippingFee.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
-                                </div>
-                                <div className="total-payment">
-                                    Tổng tiền thanh toán: {totalWithShipping.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
-                                </div>
+            <div>
+                <Form className="form-cart">
+                    <Form.Item>
+                        {listBookJson.map(item => (
+                            <ProductCard
+                                key={item.id}
+                                imageUrl={item.files[0]}
+                                title={item.title}
+                                price={item.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
+                                quantity={item.quantity}
+                                sell="15"
+                                onDelete={() => handleDeleteItem(item.id)}
+                            />
+                        ))}
+                    </Form.Item>
+                    <Form.Item>
+                        <div className="form-item-total">
+                            <div
+                                style={{
+                                    backgroundColor: 'white',
+                                    height: '50px',
+                                    fontSize: '20px',
+                                    padding: '10px',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    borderRadius: '8px',
+                                    marginBottom: '8px',
+                                    boxShadow: '0 2px 8px rgba(0, 0, 0, 0.1)',
+                                }}
+                                className="total-product"
+                            >
+                                Tổng tiền sản phẩm: {totalAmount.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
                             </div>
-                        </Form.Item>
+                            <div className="total-shipping">
+                                Phí vận chuyển: {shippingFee.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
+                            </div>
+                            <div className="total-payment">
+                                Tổng tiền thanh toán: {totalWithShipping.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })}
+                            </div>
+                        </div>
+                    </Form.Item>
 
-                        <Form.Item className="form-item-button">
-                            <Button
-                                onClick={handlePaymentOnline}
-                                type="primary"
-                                className="form-button"
-                                loading={buttonLoading} // Add the loading prop
-                            >
-                                Mua ngay <RiSecurePaymentLine />
-                            </Button>
-                        </Form.Item>
-                        <Form.Item>
-                            <Button
-                                type="default"
-                                // onClick={handleOrder}
-                                className="form-button"
-                            >
-                                Đặt hàng
-                            </Button>
-                        </Form.Item>
-                    </Form>
-                </div>
-            ) : (
-                <p>Không có thông tin đơn hàng</p>
-            )}
+                    <Form.Item className="form-item-button">
+                        <Button
+                            onClick={handlePaymentOnline}
+                            type="primary"
+                            className="form-button"
+                            loading={buttonLoading} // Add the loading prop
+                        >
+                            Mua ngay <RiSecurePaymentLine />
+                        </Button>
+                    </Form.Item>
+                    <Form.Item>
+                        <Button
+                            type="default"
+                            onClick={handlePaymentOfline}
+                            className="form-button"
+                        >
+                            Đặt hàng
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
+
         </div>
     );
 };
